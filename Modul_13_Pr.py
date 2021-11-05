b5b907a83403782c7fee04f5c36c55b473bd97df
Alexandr1003 <sarhipov97@gmail.com>
2021-11-05 20:42:44 +0300

    practice

---
+++ Modul_13_Pr.py
@@ -0,0 +1,12 @@
+numbers = int(input("Сколько билетов вы хотите приобрести: ")) #Кол-во билетов
+age = [int(input("Введите возраст: ")) for i in range(numbers)]
+print()
+free = len([x for x in age if x < 18])*0
+sale = len([x for x in age if 18 <= x < 25 ])*990
+full = len([x for x in age if x >= 25])*1390
+price = free + full + sale
+if numbers >= 3:
+    sales = int(price - price*0.1)
+    print("Сумма к оплате:" ,sales)
+else:
+    print("Сумма к оплате:" ,price)
