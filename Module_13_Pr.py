ecc307ef9af02e5900152119e8481b6cfe40676e
Alexandr1003 <sarhipov97@gmail.com>
2021-11-06 20:35:11 +0300

    1

--- Module_13_Pr.py
+++ Module_13_Pr.py
@@ -5,7 +5,7 @@ free = len([x for x in age if x < 18])*0 #Цена для лиц младше 18
 discount = len([x for x in age if 18 <= x < 25 ])*990 #Цена для лиц от 18 до 25 лет
 full = len([x for x in age if x >= 25])*1390 #Цена для лиц от 25 лет
 price = free + full + discount 
-if numbers >= 3: #Скидка 10% если больше 3 билетов
+if numbers > 3: #Скидка 10% если больше 3 билетов
     sales = int(price - price*0.1)
     print("Сумма к оплате:" ,sales)
 else: 
